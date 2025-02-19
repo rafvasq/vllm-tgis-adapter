c7c6cd4defd720f83f01e239653eb22adfed2028
Rafael Vasquez <rafvasq21@gmail.com>
2025-02-19 14:44:59 -0500

    Try something else
    Signed-off-by: Rafael Vasquez <rafvasq21@gmail.com>

--- src/vllm_tgis_adapter/logging.py
+++ src/vllm_tgis_adapter/logging.py
@@ -9,6 +9,22 @@ from vllm.logger import (
 
 DEFAULT_LOGGER_NAME = __name__.split(".")[0]
 
+config = {**DEFAULT_LOGGING_CONFIG}
+
+config["formatters"][DEFAULT_LOGGER_NAME] = DEFAULT_LOGGING_CONFIG["formatters"]["vllm"]
+config["filters"][DEFAULT_LOGGER_NAME] = DEFAULT_LOGGING_CONFIG["filters"][
+    "vllm_redact"
+]
+
+handler_config = DEFAULT_LOGGING_CONFIG["handlers"]["vllm"]
+handler_config["formatter"] = DEFAULT_LOGGER_NAME
+handler_config["filters"] = [DEFAULT_LOGGER_NAME]
+config["handlers"][DEFAULT_LOGGER_NAME] = handler_config
+
+logger_config = DEFAULT_LOGGING_CONFIG["loggers"]["vllm"]
+logger_config["handlers"] = [DEFAULT_LOGGER_NAME]
+config["loggers"][DEFAULT_LOGGER_NAME] = logger_config
+
 # Extract log filter patterns from the environment variable
 patterns = []
 vllm_log_filter_patterns = os.getenv("VLLM_LOG_FILTER_PATTERNS")
@@ -19,19 +35,6 @@ if vllm_log_filter_patterns:
     except json.JSONDecodeError:
         logging.warning("Invalid JSON format in VLLM_LOG_FILTER_PATTERNS")
 
-# Apply patterns to the DEFAULT_LOGGING_CONFIG
 DEFAULT_LOGGING_CONFIG["filters"]["vllm_redact"]["patterns"] = patterns
-
-config = {**DEFAULT_LOGGING_CONFIG}
-
-config["formatters"][DEFAULT_LOGGER_NAME] = DEFAULT_LOGGING_CONFIG["formatters"]["vllm"]
-
-handler_config = DEFAULT_LOGGING_CONFIG["handlers"]["vllm"]
-handler_config["formatter"] = DEFAULT_LOGGER_NAME
-config["handlers"][DEFAULT_LOGGER_NAME] = handler_config
-
-logger_config = DEFAULT_LOGGING_CONFIG["loggers"]["vllm"]
-logger_config["handlers"] = [DEFAULT_LOGGER_NAME]
-config["loggers"][DEFAULT_LOGGER_NAME] = logger_config
 
 logging.config.dictConfig(config)
